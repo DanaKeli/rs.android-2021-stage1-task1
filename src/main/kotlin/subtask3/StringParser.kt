ec8693fc8c192b230f2900cff16623ce36a8ef87
DanaKeli <79215094+DanaKeli@users.noreply.github.com>
2021-05-30 11:40:49 +0300

    feat:  complete String Parser task

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,37 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var arr = emptyArray<String>()
+
+        for (i in inputString.indices) {
+            var a = 1
+            when (inputString[i]) {
+                '(' -> for (j in (i + 1) until inputString.length) {
+                        if (inputString[j] == '(') a++
+                        else if (inputString[j] == ')') a--
+                    if (a == 0) {
+                        arr += inputString.substring(i + 1 until j)
+                        break
+                    }
+                }
+                '[' -> for (k in (i + 1) until inputString.length) {
+                    if (inputString[k] == '[') a++
+                    else if (inputString[k] == ']') a--
+                    if (a == 0) {
+                        arr += inputString.substring(i + 1 until k)
+                        break
+                    }
+                }
+                '<' -> for (l in (i + 1) until inputString.length) {
+                    if (inputString[l] == '<') a++
+                    else if (inputString[l] == '>') a--
+                    if (a == 0) {
+                        arr += inputString.substring(i + 1 until l)
+                        break
+                    }
+                }
+            }
+        }
+        return arr
     }
 }
