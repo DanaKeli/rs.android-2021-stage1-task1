3b0d8b330f2dbf75f2f3865612fa9443ddab48a5
DanaKeli <79215094+DanaKeli@users.noreply.github.com>
2021-05-30 11:36:48 +0300

    feat: complete HappyArray task

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,31 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+             if (sadArray.isEmpty()) {
+            return IntArray(0)
+        }
+        var verySadArray = sadArray
+        var isHappy: Boolean = false
+        var result: IntArray = IntArray(0)
+
+        while (!isHappy) {
+            isHappy = true
+            result = IntArray(verySadArray.size)
+            result[0] = verySadArray[0]
+            var currentSize = 1
+            for (i in 1 until verySadArray.size - 1) {
+                if (verySadArray[i] <= verySadArray[i - 1] + verySadArray[i + 1]) {
+                    result[currentSize] = verySadArray[i]
+                    currentSize++
+                } else {
+                    isHappy = false
+                }
+            }
+            result[currentSize] = verySadArray[verySadArray.size - 1]
+            currentSize++
+
+            verySadArray = result.sliceArray(0 until currentSize)
+        }
+        return verySadArray
     }
 }
